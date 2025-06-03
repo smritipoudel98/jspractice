7d8d8247ed6dc91e4522d587e92cc30b26cc520d
Smriti Poudel <smritipoudel0607@gmail.com>
2025-06-03 12:20:00 +0545

    destructuring and spread operator

--- js/array.js
+++ js/array.js
@@ -34,3 +34,42 @@ function sum(...numbers) {
   return numbers.map((current) => current * 2);
 }
 console.log(sum(1, 2, 3, 4, 5)); // [2, 4, 6, 8, 10]
+
+//destructuring example
+const colors = ["red", "green", "blue"];
+
+// Destructuring
+const [first, second, third] = colors;
+console.log(first); // 'red'
+console.log(second); // 'green'
+console.log(third); // 'blue'
+
+//destructuring function parameters example
+function displayColors({ primary, secondary }) {
+  console.log(`Primary color: ${primary}`);
+  console.log(`Secondary color: ${secondary}`);
+}
+const colorPalette = {
+  primary: "blue",
+  secondary: "yellow",
+};
+displayColors(colorPalette);
+
+//spread operator example
+const array1 = [1, 2, 3];
+const array2 = [4, 5, 6];
+const combinedArray = [...array1, ...array2];
+console.log(combinedArray); // [1, 2, 3, 4, 5, 6]
+
+//spread operator with objects example
+const obj1 = { a: 1, b: 2 };
+const obj2 = { c: 3, d: 4 };
+const combinedObject = { ...obj1, ...obj2 };
+console.log(combinedObject); // { a: 1, b: 2, c: 3, d: 4 }
+
+//array destructuring with rest operator example
+const numbers = [1, 2, 3, 4, 5];
+const [firstNum, secondNum, ...restOfNumbers] = numbers;
+console.log(firstNum); // 1
+console.log(secondNum); // 2
+console.log(restOfNumbers); // [3, 4, 5]
